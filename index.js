cc15f5de88b2259072f46dc645612b1c96696f51
stogether0303 <stogether0303@gmail.com>
2023-12-21 14:10:25 +0200

    d

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ function saveStatus(receiverAddy, amount) {
 
         const data = raw_data ? JSON.parse(raw_data) : defaultStatus;
         data.wallet_funded += 1;
-        data.pls_given_away += parseFloat(amount);
+        data.pls_given_away += amount * 1;
         data.balance = await getBalance(process.env.SENDER_ADDY);
 
         // Save the status
