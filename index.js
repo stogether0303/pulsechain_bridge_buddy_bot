a7920bb1c222c982c2f49a5f29e2bc865277ef20
stogether0303 <stogether0303@gmail.com>
2023-12-22 14:11:40 +0200

    fix date time issue

--- index.js
+++ index.js
@@ -143,7 +143,8 @@ async function getTokenPrice(tokenAddy) {
     }
 }
 
-function currentTime(date) {
+function currentTime() {
+    const date = new Date();
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
